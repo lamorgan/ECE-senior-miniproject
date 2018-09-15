53af19d78e26016fddbcc6908cbd2766f99521a0
lamorgan <31739310+lamorgan@users.noreply.github.com>
2018-09-14 21:57:53 -0400

    Working chart page just need to add database data

---
+++ Charts/Controllers/ChartsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Charts.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Charts.Controllers
+{
+    public class ChartsController : Controller
+    {
+        private Random rnd = new Random();
+
+        public IActionResult Line()
+        {
+            var lstModel = new List<SimpleReportViewModel>();
+           lstModel.Add( new SimpleReportViewModel
+            {
+                Day = 1,
+                Temp = 87,
+                Humidity = 30
+            } );
+            lstModel.Add( new SimpleReportViewModel
+            {
+                Day = 2,
+                Temp = 72,
+                Humidity = 10
+            } );
+            lstModel.Add( new SimpleReportViewModel
+            {
+                Day = 3,
+                Temp = 98,
+                Humidity = 70
+            } );
+            lstModel.Add( new SimpleReportViewModel
+            {
+                Day = 4,
+                Temp = 80,
+                Humidity = 30
+            } );
+            lstModel.Add( new SimpleReportViewModel
+            {
+                Day = 5,
+                Temp = 62,
+                Humidity = 20
+            } );
+            lstModel.Add( new SimpleReportViewModel
+            {
+                Day = 6,
+                Temp = 89,
+                Humidity = 80
+            } );
+            return View( lstModel );
+        }
+    }
+}
---
+++ Charts/Models/SimpleReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Charts.Models
+{
+    public class SimpleReportViewModel
+    {
+        public int Temp { get; set; }
+        public int Humidity { get; set; }
+        public int Day { get; set; }
+    }
+}
