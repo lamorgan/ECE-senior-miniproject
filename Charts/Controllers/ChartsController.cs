361e5e8dd4046b258f17acc9cdc1aaf61c06a7dc
lamorgan <31739310+lamorgan@users.noreply.github.com>
2018-09-15 14:12:37 -0400

    Working chart page - fixed some format issues

--- Charts/Controllers/ChartsController.cs
+++ Charts/Controllers/ChartsController.cs
@@ -9,8 +9,6 @@ namespace Charts.Controllers
 {
     public class ChartsController : Controller
     {
-        private Random rnd = new Random();
-
         public IActionResult Line()
         {
             string t;
@@ -30,42 +28,6 @@ namespace Charts.Controllers
                 });
                 i++;
             }
-            /*lstModel.Add( new SimpleReportViewModel
-            {
-                Day = 1,
-                Temp = 87,
-                Humidity = 30
-            } );
-            lstModel.Add( new SimpleReportViewModel
-            {
-                Day = 2,
-                Temp = 72,
-                Humidity = 10
-            } );
-            lstModel.Add( new SimpleReportViewModel
-            {
-                Day = 3,
-                Temp = 98,
-                Humidity = 70
-            } );
-            lstModel.Add( new SimpleReportViewModel
-            {
-                Day = 4,
-                Temp = 80,
-                Humidity = 30
-            } );
-            lstModel.Add( new SimpleReportViewModel
-            {
-                Day = 5,
-                Temp = 62,
-                Humidity = 20
-            } );
-            lstModel.Add( new SimpleReportViewModel
-            {
-                Day = 6,
-                Temp = 89,
-                Humidity = 80
-            } ); */
             return View( lstModel );
         }
     }
